e2b24d3e88b9ce221cf24481642907c7cfc60083
NICOLE SOUZA SANTOS <22282218@dsn127m1241959.cfp127aluno.local>
2023-09-01 16:17:04 -0300

    01/09 - activity de cadastro de categorias

---
+++ app/src/main/java/br/senai/sp/jandira/api_livraria/Cadastro_Categoria.kt
@@ -0,0 +1,11 @@
+package br.senai.sp.jandira.api_livraria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cadastro_Categoria : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_categoria)
+    }
+}
